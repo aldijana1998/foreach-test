087bc46fc3acf0e5425916248f46be64658a7991
Aldijana Zahirović <aldijanazahirovic406@gmail.com>
2022-07-22 22:05:18 -0700

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+
+    static void WriteList(ArrayList list)
+    {
+        foreach (object o in list)
+            Console.WriteLine(o);
+    }
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 10; i++)
+        {
+            list.Add(i);
+        
+        }
+        WriteList(list);
+    } 
+        }
+    
+
